64fd23cbb50eb7b7ef0efb83c9a0561c22dddab2
WalterX85 <cedric.guyot2@gmail.com>
2021-09-30 15:36:43 +0200

    pseudosclasses

---
+++ Pseudos Classes/memo.js
@@ -0,0 +1,10 @@
+/*
+Les pseudos classes:
+
+:active = change la couleur pdt le click d'un élément.
+:focus = change la couleur lors d'un click, pratique sur les inputs.
+:hover = change la couleur lors d'un survol.
+:checked =  modifie la largeur et la hauteur d'un élément lorsqu'il est cliqué.
+:visited =  change la couleur d'un lien lorsqu'il a été visité.
+:ntn-child = en paramètre on peut lui passer une position de liste (2), sélectionner un élément sur deux (2n), (even) ou (odd).
+*/
